3bf5ef86aa4fad94c30a51d81b88bf57d82091e0
Connor McQuillan <me@cpjmcquillan.com>
2023-05-18 15:49:48 +0100

    Version 0.2.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 ## [Unreleased]
 
+## [0.2.1] - 2023-05-18
+
+- No changes
+
 ## [0.2.0] - 2023-05-18
 
 - Implement basic boilerplate template
--- lib/hatate/version.rb
+++ lib/hatate/version.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Hatate
-  VERSION = "0.2.0"
+  VERSION = "0.2.1"
   RAILS_VERSION = "~> 7.0.4"
   RUBY_VERSION = IO
     .read("#{File.dirname(__FILE__)}/../../.ruby-version")
